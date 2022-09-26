847d9346fb57f56f199546bd9c21b9a445600d76
damzen <30341003+damzen@users.noreply.github.com>
2022-09-26 01:40:01 +0100

    Add files via upload

---
+++ src/Patches.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HouseLights
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(GameManager), "InstantiatePlayerObject")]
+        internal class GameManager_InstantiatePlayerObject
+        {
+            public static void Prefix()
+            {
+                /*if (!InterfaceManager.IsMainMenuActive())
+                {
+                    HouseLights.Init();
+                    HouseLights.GetSwitches();
+                }*/
+
+                // if (!InterfaceManager.IsMainMenuEnabled() && (!GameManager.IsOutDoorsScene(GameManager.m_ActiveScene) || HouseLights.notReallyOutdoors.Contains(GameManager.m_ActiveScene)))
+                if (!InterfaceManager.IsMainMenuEnabled())
+                 {
+                    HouseLights.Init();
+                    HouseLights.GetSwitches();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(AuroraElectrolizer), "Initialize")]
+        internal class AuroraElectrolizer_Initialize
+        {
+            private static void Postfix(AuroraElectrolizer __instance)
+            {
+                //if (InterfaceManager.IsMainMenuEnabled() || (GameManager.IsOutDoorsScene(GameManager.m_ActiveScene) && !HouseLights.notReallyOutdoors.Contains(GameManager.m_ActiveScene)))
+                if (InterfaceManager.IsMainMenuEnabled() || (!Settings.options.outsidelights && GameManager.IsOutDoorsScene(GameManager.m_ActiveScene)))
+                {
+                    return;
+                }
+
+                AuroraActivatedToggle[] radios = __instance.gameObject.GetComponentsInParent<AuroraActivatedToggle>();
+                AuroraScreenDisplay[] screens = __instance.gameObject.GetComponentsInChildren<AuroraScreenDisplay>();
+
+                if (radios.Length == 0 && screens.Length == 0)
+                {
+                    HouseLights.AddElectrolizer(__instance);
+                }
+
+            }
+        }
+
+        [HarmonyPatch(typeof(AuroraManager), "RegisterAuroraLightSimple", new Type[] { typeof(AuroraLightingSimple) })]
+        internal class AuroraManager_RegisterLightSimple
+        {
+            private static void Postfix(AuroraManager __instance, AuroraLightingSimple auroraLightSimple)
+            {
+                // if (InterfaceManager.IsMainMenuEnabled() || (GameManager.IsOutDoorsScene(GameManager.m_ActiveScene) && !HouseLights.notReallyOutdoors.Contains(GameManager.m_ActiveScene)))
+                if (InterfaceManager.IsMainMenuEnabled() || (!Settings.options.outsidelights && GameManager.IsOutDoorsScene(GameManager.m_ActiveScene)))
+                {
+                    return;
+                }
+
+              
+
+                HouseLights.AddElectrolizerLight(auroraLightSimple);
+            }
+        }
+
+
+        //[HarmonyPatch(typeof(AuroraManager), "Update")]
+        [HarmonyPatch(typeof(AuroraManager), "UpdateForceAurora")]
+        internal class AuroraManager_UpdateForceAurora
+        {
+            private static void Postfix(AuroraManager __instance)
+            {
+                //  if (InterfaceManager.IsMainMenuEnabled() || (GameManager.IsOutDoorsScene(GameManager.m_ActiveScene) && !HouseLights.notReallyOutdoors.Contains(GameManager.m_ActiveScene)))
+                if (InterfaceManager.IsMainMenuEnabled() || (!Settings.options.outsidelights && GameManager.IsOutDoorsScene(GameManager.m_ActiveScene)))
+                {
+                    return;
+                }
+
+                if (HouseLights.electroSources.Count > 0)
+                {
+                    HouseLights.UpdateElectroLights(__instance);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerManager), "GetInteractiveObjectDisplayText", new Type[] {typeof(GameObject)})]
+        internal class PlayerManager_GetObjText
+        {
+            private static void Postfix(PlayerManager __instance, ref string __result, GameObject interactiveObject)
+            {
+                if (interactiveObject.name == "XPZ_Switch")
+                {
+                    if (HouseLights.lightsOn)
+                    {
+                        __result = "Turn Lights Off";
+                    }
+                    else
+                    {
+                        __result = "Turn Lights On";
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerManager), "InteractiveObjectsProcessInteraction")]
+        internal class PlayerManager_InteractiveObjectsProcessInteraction
+        {
+            private static void Postfix(PlayerManager __instance, ref bool __result)
+            {
+                if (__instance.m_InteractiveObjectUnderCrosshair != null && __instance.m_InteractiveObjectUnderCrosshair.name == "XPZ_Switch")
+                {
+                    HouseLights.ToggleLightsState();
+                    GameAudioManager.PlaySound("Stop_RadioAurora", __instance.gameObject);
+
+                    Vector3 scale = __instance.m_InteractiveObjectUnderCrosshair.transform.localScale;
+                    __instance.m_InteractiveObjectUnderCrosshair.transform.localScale = new Vector3(scale.x, scale.y * -1, scale.z);
+
+                    //Play Sound
+
+                    __result = true;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(AuroraElectrolizer), "UpdateIntensity")]
+        internal class AuroraElectrolizer_UpdateIntensity
+        {
+            private static bool Prefix(AuroraElectrolizer __instance)
+            {
+                if (Settings.options.disableAuroraFlicker)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Weather), "IsTooDarkForAction", new Type[] { typeof(ActionsToBlock) })]
+        internal class Weather_IsTooDarkForAction
+        {
+            private static void Postfix(Weather __instance, ref bool __result)
+            {
+                if (__result && GameManager.GetWeatherComponent().IsIndoorScene() && HouseLights.lightsOn)
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,52 @@
+﻿using ModSettings;
+using System.IO;
+using System;
+using System.Text;
+
+namespace HouseLights
+{
+    internal class HouseLightsSettings : JsonModSettings
+    {
+        [Name("Intensity Value")]
+        [Description("Set the intensity for the lights.")]
+        [Slider(0f, 3f, 1)]
+        public float intensityValue = 2f;
+
+        [Name("Range Multiplier")]
+        [Description("Values above 1 make the lights cast light further. 2 will make them reach double the distance than default, 0 turns the lights off.")]
+        [Slider(0f, 5f, 1)]
+        public float rangeMultiplier = 1.4f;
+
+        [Name("Turn off aurora light flicker")]
+        [Description("If set to yes, aurora powered lights won't flicker and will stay on.")]
+        public bool disableAuroraFlicker = false;
+
+        [Name("Cast Shadows")]
+        [Description("If set to yes, lights will cast shadows (can show artifacts and might reduce performance)")]
+        public bool castShadows = false;
+
+        [Name("Colorless lights")]
+        [Description("If set to yes, lights will cast a more white light. If set to no, they will cast light with the default color.")]
+        public bool whiteLights = false;
+
+        [Name("Enable Outside lights")]
+        [Description("If set to yes, returns the old feature of lights outside being turned on with console cmd: Toggle_Lights (unless you set 'Default light state - ON' to 'True' below) **WARNING PERFORMANCE IMPACT due to all outdoor lights being on at once in scene!**")]
+        public bool outsidelights = false;
+
+        [Name("Default light state - ON")]
+        [Description("Sets the default state of lights in game to always being ON.")]
+        public bool outsidelightsdefault = false;
+
+    }
+
+    internal static class Settings
+    {
+        public static HouseLightsSettings options;
+
+        public static void OnLoad()
+        {
+            options = new HouseLightsSettings();
+            options.AddToModSettings("House Lights Settings");
+        }
+    }
+}
